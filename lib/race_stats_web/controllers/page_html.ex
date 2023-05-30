d32a9680e12cd87d473278aa597eb9b3730c1ee7
Kerry Buckley <kerryjbuckley@gmail.com>
2023-05-30 16:35:35 +0100

    Remove boilerplate controller and layout content

--- lib/race_stats_web/controllers/page_html.ex
+++
@@ -1,5 +0,0 @@
-defmodule RaceStatsWeb.PageHTML do
-  use RaceStatsWeb, :html
-
-  embed_templates "page_html/*"
-end
--- lib/race_stats_web/router.ex
+++ lib/race_stats_web/router.ex
@@ -2,22 +2,20 @@ defmodule RaceStatsWeb.Router do
   use RaceStatsWeb, :router
 
   pipeline :browser do
-    plug :accepts, ["html"]
-    plug :fetch_session
-    plug :fetch_live_flash
-    plug :put_root_layout, {RaceStatsWeb.Layouts, :root}
-    plug :protect_from_forgery
-    plug :put_secure_browser_headers
+    plug(:accepts, ["html"])
+    plug(:fetch_session)
+    plug(:fetch_live_flash)
+    plug(:put_root_layout, {RaceStatsWeb.Layouts, :root})
+    plug(:protect_from_forgery)
+    plug(:put_secure_browser_headers)
   end
 
   pipeline :api do
-    plug :accepts, ["json"]
+    plug(:accepts, ["json"])
   end
 
   scope "/", RaceStatsWeb do
-    pipe_through :browser
-
-    get "/", PageController, :home
+    pipe_through(:browser)
   end
 
   # Other scopes may use custom stacks.
@@ -35,10 +33,10 @@ defmodule RaceStatsWeb.Router do
     import Phoenix.LiveDashboard.Router
 
     scope "/dev" do
-      pipe_through :browser
+      pipe_through(:browser)
 
-      live_dashboard "/dashboard", metrics: RaceStatsWeb.Telemetry
-      forward "/mailbox", Plug.Swoosh.MailboxPreview
+      live_dashboard("/dashboard", metrics: RaceStatsWeb.Telemetry)
+      forward("/mailbox", Plug.Swoosh.MailboxPreview)
     end
   end
 end
